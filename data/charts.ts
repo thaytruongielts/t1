95e5e6792d2ec5f88a4cffa802c9b4dfdebb39c2
thaytruongielts <backuptruong@gmail.com>
2025-10-24 21:54:03 +0700

    Create charts.ts

---
+++ data/charts.ts
@@ -0,0 +1,29 @@
+import type { Chart } from '../types';
+
+export const CHARTS: Chart[] = [
+  {
+    id: 1,
+    title: "The bar chart below shows how commuters in the UK travelled to work in 1990 and 2020.",
+    image: "https://i.imgur.com/v1nL7M8.png"
+  },
+  {
+    id: 2,
+    title: "The line graph below shows the percentage of people in a European country who went to the cinema from 1980 to 2020.",
+    image: "https://i.imgur.com/2s9nL6C.png"
+  },
+  {
+    id: 3,
+    title: "The pie charts below show the main reasons why people chose to study at a particular UK university in 1995 and 2015.",
+    image: "https://i.imgur.com/pYxYc8B.png"
+  },
+  {
+    id: 4,
+    title: "The table below gives information about underground railway systems in six cities.",
+    image: "https://i.imgur.com/GzB1y4N.png"
+  },
+   {
+    id: 5,
+    title: "The diagram below shows the process of making recycled paper.",
+    image: "https://i.imgur.com/tFvFzex.png"
+  }
+];
